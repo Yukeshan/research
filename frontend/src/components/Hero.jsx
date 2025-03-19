c657915b842c2acfe462815a77ec21ef6826071e
Yukeshan <yjyukesh@gmail.com>
2025-03-19 22:39:35 +0530

    backend test

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="px-5 xl:px-10 md:w-1/2 mb-10">
         <h1 className="mt-6 mb-10 text-5xl xl:text-6xl text-center font-bold text-[#2A3342] leading-normal xl:leading-relaxed">
-        A blog template made for food influencers
+        Let's Enjoy Cooking With Cook It
         </h1>
         <form action="/search" className="bg-white p-4 rounded relative flex items-center ">
         <IoSearchOutline className='w-5 h-5 mr-2 text-neutral-300'/>
